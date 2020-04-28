c5a288c19f3dda79a95b14342d4c18355c5f4bf4
trgianglotus <hoangtruonggiangnguyen@gmail.com>
2020-04-28 16:28:42 +0800

    add form and list components

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import './App.css';
+import TodoList from './todos/TodoList';
+
+const App = () => (
+    <div className="App">
+        <TodoList />
+    </div>
+);
+
+export default hot(module)(App);
